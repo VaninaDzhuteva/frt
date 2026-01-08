14b319ba554b050bbfc57a93fd45273693805327
vdzhuteva <v.k.dzhuteva@gmail.com>
2026-01-08 12:01:34 +0200

    feat: update package.json for development and build tools
    - Added a new script for development using webpack-dev-server.
    - Replaced node-sass with sass for SCSS compilation.
    
    style: update button styles and form input placeholder
    
    - Added font-family to button styles.
    - Adjusted font size of form input placeholder.
    
    style: center align text in index page
    
    - Added text-align center to the main heading in index page styles.
    
    fix: improve swiper initialization and event listeners
    
    - Wrapped swiper initialization in a conditional to check for existence of the container.
    - Added checks for the existence of elements before adding event listeners for checkbox toggle and menu item activation.

--- src/scripts/ui.js
+++ src/scripts/ui.js
@@ -1,29 +1,38 @@
-var mySwiper = new Swiper('.swiper-container', {
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
+if (document.querySelector('.swiper-container')) {
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    }
-});
+    var mySwiper = new Swiper('.swiper-container', {
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        }
+    });
+}
+
 
 //Checkbox toggle
-document.getElementById('checkbox').addEventListener('click', () => {
-    document.querySelector('.bg-check').classList.toggle('hidden');
-});
+if (document.querySelector('.bg-check')) {
+    document.getElementById('checkbox').addEventListener('click', () => {
+        document.querySelector('.bg-check').classList.toggle('hidden');
+    });
+}
 
 // Toggle active class
-const menuItem = document.querySelectorAll('.nav-item ');
+if (document.querySelector('.nav-item')) {
+    const menuItem = document.querySelectorAll('.nav-item');
 
-for (let i = 0; i < menuItem.length; i++) {
-    menuItem[i].addEventListener('click', function () {
-        const current = document.getElementsByClassName('active');
-        current[0].className = current[0].className.replace(' active', '');
-        this.className += ' active';
-    });
+    for (let i = 0; i < menuItem.length; i++) {
+        menuItem[i].addEventListener('click', function () {
+            const current = document.getElementsByClassName('active');
+            current[0].className = current[0].className.replace(' active', '');
+            this.className += ' active';
+        });
+    }
+
 }
